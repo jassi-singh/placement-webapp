c5863fc747a1590c7a3cc6d187cafee653662785
Jassi <jaswinder.dandyan2000@gmail.com>
2022-12-08 16:44:38 +0530

    Admin app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getAuth } from "firebase/auth";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useAppDispatch } from "./app/hooks";
+import AdminLayout from "./components/AdminLayout";
+import Layout from "./components/Layout";
+import { setUser } from "./features/auth/authSlice";
+import app from "./firebase";
+import Dashboard from "./pages/Dashboard";
+import Drives from "./pages/Drives";
+import Landing from "./pages/Landing";
+import ProfileForm from "./pages/ProfileForm";
+import SignupPage from "./pages/SignupPage";
+import Students from "./pages/Students";
 
 function App() {
+  const auth = getAuth(app);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    return auth.onAuthStateChanged((user) => {
+      dispatch(setUser());
+    });
+  }, [auth, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+        </Route>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profileForm" element={<ProfileForm />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/drives" element={<Drives />} />
+          <Route path="/admin/students" element={<Students />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
